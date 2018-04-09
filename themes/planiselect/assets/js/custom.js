686b27e60106d32cbe4966bd51deea5f457c0b99
Félix <the2deux@gmail.com>
2018-04-09 11:56:37 -0400

    Fixed hashing JS

--- themes/planiselect/assets/js/custom.js
+++ themes/planiselect/assets/js/custom.js
@@ -58,22 +58,24 @@ $(document).ready(function(){
 });
 
 $(function() {
-  setTimeout(function() {
-    if (location.hash) {
-      /* we need to scroll to the top of the window first, because the browser will always jump to the anchor first before JavaScript is ready, thanks Stack Overflow: http://stackoverflow.com/a/3659116 */
-      window.scrollTo(0, 0);
-      target = location.hash.split('#');
-      smoothScrollTo($('#'+target[1]));
-    }
-  }, 1);
-  
-  function smoothScrollTo(target) {
-    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+  if (location.pathname.indexOf('/services/cablage') == 0 || location.pathname.indexOf('/en/become-a-franchisee') == 0) {
+    setTimeout(function() {
+      if (location.hash) {
+        /* we need to scroll to the top of the window first, because the browser will always jump to the anchor first before JavaScript is ready, thanks Stack Overflow: http://stackoverflow.com/a/3659116 */
+        window.scrollTo(0, 0);
+        target = location.hash.split('#');
+        smoothScrollTo($('#'+target[1]));
+      }
+    }, 1);
     
-    if (target.length) {
-      $('html,body').animate({
-        scrollTop: target.offset().top - 70
-      }, 1000);
+    function smoothScrollTo(target) {
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 70
+        }, 1000);
+      }
     }
   }
 });
